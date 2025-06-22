35c5c41cd619a02c35202fc401415789c3c07df4
Ranay <54890063+ranarajput123@users.noreply.github.com>
2025-06-22 18:18:30 +0100

    fix: fix condition

--- terraform/modules/teams/main.tf
+++ terraform/modules/teams/main.tf
@@ -7,7 +7,7 @@ resource "google_project_iam_custom_role" "full_stack_dev" {
 }
 
 resource "google_project_iam_member" "full_stack_dev" {
-  for_each = length(var.full_stack_devs) > 0 ? toset(var.full_stack_devs) : {}
+  for_each = length(var.full_stack_devs) > 0 ? toset(var.full_stack_devs) : toset([])
 
   project  = var.project
   role     = google_project_iam_custom_role.full_stack_dev[0].id
@@ -23,7 +23,7 @@ resource "google_project_iam_custom_role" "backend_dev" {
 }
 
 resource "google_project_iam_member" "backend_dev" {
-  for_each = length(var.backend_devs) > 0 ? toset(var.backend_devs) : {}
+  for_each = length(var.backend_devs) > 0 ? toset(var.backend_devs) : toset([])
   
   project  = var.project
   role     = google_project_iam_custom_role.backend_dev[0].id
@@ -39,7 +39,7 @@ resource "google_project_iam_custom_role" "frontend_dev" {
 }
 
 resource "google_project_iam_member" "frontend_dev" {
-  for_each = length(var.frontend_devs) > 0 ? toset(var.frontend_devs) : {}
+  for_each = length(var.frontend_devs) > 0 ? toset(var.frontend_devs) : toset([])
 
   project  = var.project
   role     = google_project_iam_custom_role.frontend_dev[0].id
