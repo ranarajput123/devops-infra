da2a624570cdc5ca7ebc6b0630f87a73f55b50cc
Ranay <54890063+ranarajput123@users.noreply.github.com>
2025-06-23 23:52:56 +0100

    fix kubeconfig

--- terraform/modules/flux/main.tf
+++ terraform/modules/flux/main.tf
@@ -21,9 +21,10 @@ resource "github_repository_deploy_key" "gitops_deploy_key" {
 # Adding provider block here because of dependency on ssh key
 provider "flux" {
   kubernetes = {
-    host                   = var.api_server_endpoint
-    cluster_ca_certificate = var.b64_ca_cert
-    token                  = var.gcp_access_token
+    # host                   = var.api_server_endpoint
+    # cluster_ca_certificate = var.b64_ca_cert
+    # token                  = var.gcp_access_token
+    config_path = "~/.kube/config"
   }
   git = {
     url = "ssh://git@github.com/${var.github_owner}/${var.gitops_repo_name}.git"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,3 +7,9 @@ provider "github" {
   token = data.google_secret_manager_secret_version.github_token.secret_data
   owner = var.github_owner
 }
+
+provider "kubernetes" {
+  host                   = "https://${module.gke_cluster.api_server_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke_cluster.b64_ca_cert)
+}
