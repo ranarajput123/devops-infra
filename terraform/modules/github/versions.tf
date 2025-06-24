5dc44664e885b2d3bd298148e43d83ea7a27a2fc
Ranay <54890063+ranarajput123@users.noreply.github.com>
2025-06-24 16:41:07 +0100

    fix git repo url issue by adding required provider block

--- terraform/modules/github/versions.tf
+++ terraform/modules/github/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.10.4"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.40.0"
+    github = {
+      source  = "integrations/github"
+      version = "6.6.0"
     }
   }
 }
