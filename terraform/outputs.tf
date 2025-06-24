ddac03e67d341b59b5005f9e3de96cb42ed29f75
Ranay <54890063+ranarajput123@users.noreply.github.com>
2025-06-24 03:12:09 +0100

    asd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,7 @@ output "gke_api_server_endpoint" {
   description = "The API server endpoint of the GKE cluster"
   value       = "https://${module.gke_cluster.api_server_endpoint}"
 }
+
+output "cluster_ca_cert" {
+  value = module.gke_cluster.b64_ca_cert
+}
