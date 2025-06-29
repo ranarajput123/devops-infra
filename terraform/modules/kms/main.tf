01b7aab6d263d51b62fad4e6d5bf8fdb0ebadcd6
Ranay <54890063+ranarajput123@users.noreply.github.com>
2025-06-29 02:13:26 +0100

    patch: lock providers

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -1,19 +1,19 @@
-resource "google_kms_key_ring" "key_ring" {
-  name     = var.key_ring_name
-  location = var.region
+# resource "google_kms_key_ring" "key_ring" {
+#   name     = var.key_ring_name
+#   location = var.region
 
-  lifecycle {
-    prevent_destroy = false
-  }
+#   lifecycle {
+#     prevent_destroy = false
+#   }
 
-}
+# }
 
-resource "google_kms_crypto_key" "crypto_key" {
-  name     = var.key_name
-  key_ring = google_kms_key_ring.key_ring.id
+# resource "google_kms_crypto_key" "crypto_key" {
+#   name     = var.key_name
+#   key_ring = google_kms_key_ring.key_ring.id
 
-  lifecycle {
-    prevent_destroy = false
-  }
-  destroy_scheduled_duration = "86400s" # 1 day, lowest value allowed by GCP
-}
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+#   destroy_scheduled_duration = "86400s" # 1 day, lowest value allowed by GCP
+# }
--- terraform/modules/kms/outputs.tf
+++ terraform/modules/kms/outputs.tf
@@ -1,7 +1,7 @@
-output "key_ring_id" {
-  value = google_kms_key_ring.key_ring.id
-}
+# output "key_ring_id" {
+#   value = google_kms_key_ring.key_ring.id
+# }
 
-output "crypto_key_id" {
-  value = google_kms_crypto_key.crypto_key.id
-}
+# output "crypto_key_id" {
+#   value = google_kms_crypto_key.crypto_key.id
+# }
